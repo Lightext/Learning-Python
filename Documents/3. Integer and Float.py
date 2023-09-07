3af4c86ab290a89db6c08293a321d3136f4a19bf
Ozan <85025689+Lightext@users.noreply.github.com>
2023-09-07 20:58:23 +0300

    Add files via upload

---
+++ Documents/3. Integer and Float.py
@@ -0,0 +1,7 @@
+type(3)
+# int
+type(1.5)
+# float
+
+print("Integers are of data type int, and decimal numbers are of data type float.")
+print("When giving decimal values to numbers, a period is used instead of a comma.")
