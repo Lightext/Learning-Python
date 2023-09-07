081c6e952865246b869b3b95266bfeffba9e590b
Ozan <ozansans2000@gmail.com>
2023-09-07 22:42:55 +0300

    Add files via upload

---
+++ Documents/5. Boolean.py
@@ -0,0 +1,20 @@
+print("You can only give a True or False value to the Boolean data type.")
+
+a = True
+b = False
+print(a)
+print(b)
+
+print(type(a))
+print(type(b))
+
+c = "True"
+print(type(c))
+
+age1 = 20
+age2 = 18
+print(age2 > 18)
+print(age2 == 18)
+print(age2 != 18)
+print(not age2 == "8")
+print(not age2 > 18)
