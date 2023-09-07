d03c4b71aa86509ceb140bab81e07e497bca30f6
Ozan <85025689+Lightext@users.noreply.github.com>
2023-09-07 20:27:59 +0300

    Add files via upload

---
+++ Documents/2. Assigning a Variable.py
@@ -0,0 +1,21 @@
+number = 10
+print(number)
+
+number = 11
+print(number)
+
+number = number + 1
+print(number)
+
+first_number = 10
+print(first_number)
+
+print("You cannot specify a number per variable.")
+print("No symbol other than the underscore is used as a variable. Nor can it be used in a variable.")
+
+_ = 'hallo'
+print(_)
+# You can assign the "_" variable to values that you will not use permanently.
+
+print("'Camel Case' can be used for variable assignments.")
+studentNo = 593
