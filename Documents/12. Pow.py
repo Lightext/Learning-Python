57d3dd0bc306805a6102a579ee7eb796392826d4
Ozan <ozansans2000@gmail.com>
2023-09-08 13:23:19 +0300

    Add files via upload

---
+++ Documents/12. Pow.py
@@ -0,0 +1,5 @@
+a = pow(12,2)
+print(a)
+
+b = pow(12,2,3)
+print(b)
---
+++ Documents/13. Sep.py
@@ -0,0 +1,16 @@
+print("www",".","google",".","com",sep=" ")
+
+print("www",".","google",".","com",sep="")
+
+print("www",".","google",".","com",sep="*")
+
+print("www",".","google",".","com",sep=None)
+
+
+print(*"Linux")
+
+print(*"Linux", sep="_")
+
+print(*"TBMM", sep=".")
+
+print(*"abcdefgh", sep="/")
